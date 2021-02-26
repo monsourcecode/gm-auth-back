eaa0b0db7d23ebd1488a2d2fe1412821e3493d10
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-02-26 16:48:19 +0100

    first commit

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const {Login, SignUp, AllUsers,Uplaod} = require("../controllers/user-controller");
+const {AddItem} = require("../controllers/item-controller");
+const {Items} = require("../controllers/item-controller");
+const {Login, SignUp, AllUsers, Uplaod} = require("../controllers/user-controller");
 
 const router = express.Router();
 
 router.post('/login', Login)
-router.post('/signup',SignUp)
-router.get('/users',AllUsers)
-router.post('/upload',Uplaod)
-module.exports=router
+router.post('/signup', SignUp)
+router.get('/users', AllUsers)
+router.post('/upload', Uplaod)
+router.post('/additem', AddItem)
+router.get('/items', Items)
+
+module.exports = router
 
